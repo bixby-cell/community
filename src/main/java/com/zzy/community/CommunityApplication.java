0f9471bee695b6e77cacc7fab02ee49e9dfb9220
bixby <1439381768@qq.com>
2020-05-05 15:48:22 +0800

    init repo

---
+++ src/main/java/com/zzy/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package com.zzy.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 14393
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
